2b6b3c6d1b583664d7f903c7174a7abdfa43e71d
後藤恭智 <yasutomo.goto@xenophy.com>
2015-09-25 13:59:20 +0900

    削除処理の追加と種類列の追加

--- src/js/simple.js
+++ src/js/simple.js
@@ -12,21 +12,27 @@ var app = new Vue({
             address: '北海道札幌市西区山の手１条１３丁目５−１７',
             range: 100,
             uid: 0,
-            display: true
+            display: true,
+            type: "1",
+            delflg: false
         }, {
             lat: 43.06913858232447,
             lng: 141.34576320648193,
             address: '北海道札幌市北区北７条西６丁目２−８',
             range: 200,
             uid: 1,
-            display: true
+            display: true,
+            type: "1",
+            delflg: false
         }, {
             lat: 43.043927231600534,
             lng: 141.4209508895874,
             address: '北海道札幌市白石区本通（北）９丁目北４−８',
             range: 250,
             uid: 2,
-            display: true
+            display: true,
+            type: "1",
+            delflg: false
         }],
         maxuid: 2
     },
@@ -37,9 +43,12 @@ var app = new Vue({
         'add:location': '_addLocation',
         'add:list': '_addLocation',
         'change:display': '_changeDisplay',
-        'change:range': '_changeRange'
+        'change:range': '_changeRange',
+        'change:type': '_onChangeType',
+        'click:delbtn': '_removeLocation'
     },
     methods: {
+
         _addLocation: function(latlng, uid) {
             var me = this;
             this._useGoogleApi(latlng).done(function(data) {
@@ -50,12 +59,13 @@ var app = new Vue({
                     address: address,
                     range: 200,
                     uid: uid,
-                    display: true
+                    display: true,
+                    type: "1",
+                    delflg: false
                 });
             });
         },
-        _removeLocation: function(e) {
-        },
+
         _useGoogleApi: function(latlng) {
             var defer = $.Deferred();
             $.ajax({
@@ -74,9 +84,28 @@ var app = new Vue({
         _changeDisplay: function(location) {
             this.$broadcast('change:display', location);
         },
+
         _changeRange: function(location) {
             this.$broadcast('change:range', location);
+        },
+
+        _onChangeType: function(location) {
+            this.$broadcast('change:type', location);
+        },
+
+        _removeLocation: function() {
+
+            var delLocs = this.locations.filter(function(item) {
+                return (item.delflg);
+            });
+
+            this.locations = this.locations.filter(function(item) {
+               return !(item.delflg);
+            });
+
+            this.$broadcast('click:delbtn', delLocs);
         }
+
     },
     components: {
         props: ['locations'],
@@ -116,7 +145,9 @@ var app = new Vue({
             events: {
                 'root:ready': '_addCircles',
                 'change:display': '_changeDisplay',
-                'change:range': '_changeRange'
+                'change:range': '_changeRange',
+                'change:type': '_changeType',
+                'click:delbtn': '_removeCircles'
             },
             methods: {
 
@@ -128,7 +159,8 @@ var app = new Vue({
 
                 _addCircle: function(latlng, uid) {
                     var opt = {
-                            uid: uid
+                            uid: uid,
+                            color: '#2979ff'
                         },
                         circle = L.circle([latlng.lat, latlng.lng], 200, opt);
                     this.map.addControl(circle);
@@ -178,6 +210,50 @@ var app = new Vue({
                         }
 
                     }
+                },
+
+                _changeType: function(location) {
+
+                    for (var i = 0, len = this.circles.length; i < len; i++) {
+
+                        if (location.uid === this.circles[i].options.uid) {
+
+                            if (location.type === "1") {
+                                this.circles[i].setStyle({color: '#2979ff'});
+                            } else if (location.type === "2") {
+                                this.circles[i].setStyle({color: '#ff1744'});
+                            } else {
+                                this.circles[i].setStyle({color: '#00e676'});
+                            }
+
+                        }
+
+                    }
+
+                },
+
+                _removeCircles: function(delLocs) {
+
+                    for (var i = 0, dlen = delLocs.length; i < dlen; i++) {
+
+                        var delUid = delLocs[i].uid;
+
+                        for(var j = 0, clen = this.circles.length; j < clen; j++) {
+
+                            if (delUid === this.circles[j].options.uid) {
+
+                                this.map.removeLayer(this.circles[j]);
+                                break;
+
+                            }
+
+                        }
+
+                    }
+
+
+
+
                 }
 
             },
@@ -188,15 +264,33 @@ var app = new Vue({
 
         list: {
             props: ['locations'],
+            events: {
+                'root:ready': '_onReady'
+            },
             methods: {
+
                 _onClickAddBtn: function() {
                     this.$dispatch('add:list');
                 },
+
                 _onChangeRange: function(location) {
                     this.$dispatch('change:range', location);
                 },
+
                 _onChangeDispay: function(location) {
                     this.$dispatch('change:display', location);
+                },
+
+                _onChangeType: function(location) {
+                    this.$dispatch('change:type', location);
+                },
+
+                _onReady: function() {
+                    $('select').material_select();
+                },
+
+                _onClickDeleteBtn: function() {
+                    this.$dispatch('click:delbtn');
                 }
 
             },
@@ -206,6 +300,7 @@ var app = new Vue({
                     '<thead>' +
                         '<tr>' +
                             '<th>削除</th>' +
+                            '<th>種類</th>' +
                             '<th>住所</th>' +
                             '<th>範囲</th>' +
                             '<th>表示</th>' +
@@ -213,7 +308,8 @@ var app = new Vue({
                     '</thead>' +
                     '<tbody>' +
                         '<tr v-repeat="locations">' +
-                            '<td><p><input type="checkbox" id="{{uid}}" /><label for="{{uid}}"></label></p></td>' +
+                            '<td><p><input type="checkbox" id="{{uid}}" v-model="delflg" /><label for="{{uid}}"></label></p></td>' +
+                            '<td><select class="browser-default" v-model="type" v-on="change: _onChangeType(this)"><option value="1">飲食店</option><option value="2">病院</option><option value="3">公園</option></select></td>' +
                             '<td>{{address}}</td>' +
                             '<td><div class="range-field"><input type="range" min="50" max="400" v-model="range" v-on="change: _onChangeRange(this)" /><span class="thumb"><span class="value"></span></span></div></td>' +
                             '<td><div class="switch"><label><input type="checkbox" v-on="change: _onChangeDispay(this)" v-model="display" /><span class="lever"></span></label></div></td>' +
@@ -221,7 +317,7 @@ var app = new Vue({
                     '</tbody>' +
                     '<tfoot>' +
                         '<tr>' +
-                            '<td colspan="6"><a class="btn-floating btn-large waves-effect waves-light red right"><i class="material-icons">delete</i></a><a class="btn-floating btn-large waves-effect waves-light right" v-on="click: _onClickAddBtn"><i class="material-icons">add</i></a></td>' +
+                            '<td colspan="6"><a class="btn-floating btn-large waves-effect waves-light red right" v-on="click: _onClickDeleteBtn"><i class="material-icons">delete</i></a><a class="btn-floating btn-large waves-effect waves-light right" v-on="click: _onClickAddBtn"><i class="material-icons">add</i></a></td>' +
                         '</tr>' +
                     '</tfoot>' +
                 '</table>' +
